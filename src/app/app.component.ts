f0797a99bc08bde1388e913d9cb284c1d6265c99
snash-0 <87521471+snash-0@users.noreply.github.com>
2022-11-24 19:43:11 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,51 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+
+  inputNameField = '';
+  inputeDateField: Date = null;
+  inputTextField = '';
+
+  members: string[] = [];
+
+  myObjArray = [
+    {
+      name: '',
+      date: null,
+      Text: '',
+    },
+  ];
+
+  addInputName(Name) {
+    //console.log(Name);
+    this.inputNameField = Name;
+  }
+  addInputDate(Date) {
+    //console.log(Date);
+    this.inputeDateField = Date;
+  }
+  addInputText(Text) {
+    //console.log(Text);
+    this.inputTextField = Text;
+  }
+  AddNotes() {
+    //console.log(
+    //this.inputNameField +
+    //' + ' +
+    //this.inputeDateField +
+    //' + ' +
+    //this.inputTextField
+    //);
+    this.myObjArray.push({
+      name: this.inputNameField,
+      date: this.inputeDateField,
+      Text: this.inputTextField,
+    });
+    
+    //console.log(this.myObjArray);
+  }
 }
